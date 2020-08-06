56f1efe283eab48169a70aabca13ce78008d3104
Andrey <wugandrey@gmail.com>
2020-08-06 19:26:41 +0400

    added buttons next and prev slide

--- src/components/sliderWrapp/sliderWrapp.js
+++ src/components/sliderWrapp/sliderWrapp.js
@@ -1,55 +1,62 @@
-import React from 'react';
-import './sliderWrapper.css';
+import React from "react";
+import "./sliderWrapper.css";
 
-let arr=['./images/1.jpg','./images/2.jpg','./images/3.jpg'];
+let arr = ["./images/1.jpg", "./images/2.jpg", "./images/3.jpg"];
 let i = 0;
 
 class SliderWrapp extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {image: arr[i],
-                  speed: 1};
-    this.faster = this.faster.bind(this)
-  }
-  
-  faster(){
-    this.setState((prevState, props) => ({
-      speed: prevState.speed / 2
-    }));
+    this.state = { image: arr[i] };
+    this.next = this.next.bind(this);
+    this.prev = this.prev.bind(this);
   }
 
   componentDidMount() {
-    this.timerID = setInterval(() => this.next(),2000 * this.state.speed);
+    this.slider();
   }
 
-  componentWillUnmount() {
-    clearInterval(this.timerID);
-  }  
+  slider() {
+    this.idInterval = setInterval(() => this.next(), 3000);
+  }
+
   next() {
-      if(i < arr.length-1){
-          i++;
-          this.setState({
-              image: arr[i],
-              speed: this.state.speed
-          });
-      }else{
-          this.setState({
-              image: arr[0]
-          });
-          i = 0;
-      }
+    if (i < arr.length - 1) {
+      i++;
+      this.setState({ image: arr[i] });
+    } else {
+      this.setState({ image: arr[0] });
+      i = 0;
+    }
+    clearInterval(this.idInterval);
+    this.slider();
+  }
+  prev() {
+    if (i >= 1) {
+      i--;
+      this.setState({
+        image: arr[i],
+      });
+    } else {
+      this.setState({
+        image: arr[arr.length - 1],
+      });
+      i = arr.length - 1;
+    }
+    clearInterval(this.idInterval);
+    this.slider();
   }
 
   render() {
     return (
       <div>
-          <div className="slider__wrapper">
-              <img className="slider__images" src={this.state.image}/>
-          </div>
-          <div className="btn-wrapper">            
-            <button onClick={this.faster}>+</button>
-            <button>-</button>
-          </div>
+        <div className="slider__wrapper">
+          <img className="slider__images" src={this.state.image} />
+        </div>
+        <div className="btn-wrapper">
+          <button onClick={this.prev}>-</button>
+          <button onClick={this.next}>+</button>
+        </div>
       </div>
     );
   }
