de5b409532e5abc72979db00c8d37f00df46bc6f
Andrey <wugandrey@gmail.com>
2020-08-07 18:11:03 +0400

    added redux

--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import {Provider} from 'react-redux';
 import * as serviceWorker from './serviceWorker';
+import { render } from 'node-sass';
+import rootReducer from './redux/rootReducer';
+import createStore from './redux/createStore';
 
-ReactDOM.render(
-  <React.StrictMode>
-  <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const store = createStore(rootReducer, 0)
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//   <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+const app = (
+  <Provider store={store}>
+      <App />
+  </Provider>
+)
+render(app, document.getElementById('root'));
 
 serviceWorker.unregister();
